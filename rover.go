bae73aced79b0772039ba0228114ab0e90f71a92
andys <andysmith836@gmail.com>
2020-09-17 23:01:44 +0100

    Going east and wrapping at end of world

--- rover.go
+++ rover.go
@@ -5,16 +5,49 @@ import (
 )
 
 func Execute(commands string) string {
+	x := 0
 	y := 0
+	directionIndex := 0
+	direction := "N"
 
 	for _, command := range commands {
+		if command == 'R' {
+			directionIndex++
+			if directionIndex > 3 {
+				directionIndex = 0
+			}
+		} else if command == 'L' {
+			directionIndex--
+			if directionIndex < 0 {
+				directionIndex = 3
+			}
+		}
+
+		if directionIndex == 0 {
+			direction = "N"
+		} else if directionIndex == 1 {
+			direction = "E"
+		} else if directionIndex == 2 {
+			direction = "S"
+		} else if directionIndex == 3 {
+			direction = "W"
+		}
+
 		if command == 'M' {
-			y++
-			if y > 9 {
-				y = 0
+			if direction == "E" {
+				x++
+				if x > 9 {
+					x = 0
+				}
+			} else {
+
+				y++
+				if y > 9 {
+					y = 0
+				}
 			}
 		}
 
 	}
-	return fmt.Sprintf("0:%d:N", y)
+	return fmt.Sprintf("%d:%d:%s", x, y, direction)
 }
--- rover_test.go
+++ rover_test.go
@@ -18,6 +18,32 @@ func TestWrapsIfGoingNorth(t *testing.T) {
 	assertEquals(t, "0:0:N", Execute("MMMMMMMMMM"))
 }
 
+func TestRotatingRight(t *testing.T) {
+	assertEquals(t, "0:0:E", Execute("R"))
+	assertEquals(t, "0:0:S", Execute("RR"))
+	assertEquals(t, "0:0:W", Execute("RRR"))
+	assertEquals(t, "0:0:N", Execute("RRRR"))
+	assertEquals(t, "0:0:E", Execute("RRRRR"))
+}
+
+func TestRotatingLeft(t *testing.T) {
+	assertEquals(t, "0:0:W", Execute("L"))
+	assertEquals(t, "0:0:S", Execute("LL"))
+	assertEquals(t, "0:0:E", Execute("LLL"))
+	assertEquals(t, "0:0:N", Execute("LLLL"))
+	assertEquals(t, "0:0:W", Execute("LLLLL"))
+}
+
+func TestMovingEast(t *testing.T) {
+	assertEquals(t, "1:0:E", Execute("RM"))
+	assertEquals(t, "3:0:E", Execute("RMMM"))
+}
+
+func TestWrapsIfGoingEast(t *testing.T) {
+	assertEquals(t, "0:0:E", Execute("RMMMMMMMMMM"))
+	assertEquals(t, "2:0:E", Execute("RMMMMMMMMMMMM"))
+}
+
 func assertEquals(t *testing.T, expected string, result string) {
 	if result != expected {
 		t.Errorf("Expected '%s' but got '%s'", expected, result)
