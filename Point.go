f73e5765ac6d564c1caee13844285d690b411ef0
andys <andysmith836@gmail.com>
2020-09-25 22:07:41 +0100

    replace everything with position addition

--- Point.go
+++ Point.go
@@ -14,28 +14,27 @@ func (pos Point) Add(other Point) Point {
 
 func (pos *Point) moveEast() {
 	*pos = pos.Add(Point{1, 0})
-	//pos.x++
 	if pos.x > 9 {
 		pos.x = 0
 	}
 }
 
 func (pos *Point) moveWest() {
-	pos.x--
+	*pos = pos.Add(Point{-1, 0})
 	if pos.x < 0 {
 		pos.x = 9
 	}
 }
 
 func (pos *Point) moveSouth() {
-	pos.y--
+	*pos = pos.Add(Point{0, -1})
 	if pos.y < 0 {
 		pos.y = 9
 	}
 }
 
 func (pos *Point) moveNorth() {
-	pos.y++
+	*pos = pos.Add(Point{0, 1})
 	if pos.y > 9 {
 		pos.y = 0
 	}
