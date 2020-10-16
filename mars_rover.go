3489aef8caaa012239a2325a2f9e2c20c5e2ceab
andys <andysmith836@gmail.com>
2020-10-16 21:42:33 +0100

    Use look up table for functions

--- mars_rover.go
+++ mars_rover.go
@@ -3,14 +3,21 @@ package rover
 func Execute(commands string) string {
 	grid := Grid{maxX: 9, maxY: 9}
 	var rover = Rover{grid: grid}
+
 	for _, command := range commands {
-		if command == 'R' {
-			rover.turnRight()
-		} else if command == 'L' {
-			rover.turnLeft()
-		} else if command == 'M' {
-			rover.move()
-		}
+		run(&rover, command)
 	}
 	return rover.String()
+}
+
+func run(rover *Rover, command int32) {
+	createCommands(rover)[command]()
+}
+
+func createCommands(rover *Rover) map[int32]func() {
+	m := make(map[int32]func())
+	m['R'] = rover.turnRight
+	m['L'] = rover.turnLeft
+	m['M'] = rover.move
+	return m
 }
